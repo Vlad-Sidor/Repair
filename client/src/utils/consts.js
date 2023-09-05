5cdfe76df5c3ecdab5e747a15f90c04497b8856e
Vlad-Sidor <52883583+Vlad-Sidor@users.noreply.github.com>
2023-09-05 20:36:38 +0300

    feat: Delete client/src directory

--- client/src/utils/consts.js
+++
@@ -1,5 +0,0 @@
-export const ADMIN_ROUTE = '/admin'
-export const LOGIN_ROUTE = '/login'
-export const REGISTRATION_ROUTE = '/registration'
-export const SHOP_ROUTE = '/'
-export const DEVICE_ROUTE = '/serviceinfo'
